c15e18992ca019378732cfe8926f963ae5907c10
Niklas Weber <nweber@student.42Heilbronn.de>
2025-08-26 14:08:02 +0200

    adjusted error messages

--- includes/error.h
+++ includes/error.h
@@ -13,9 +13,12 @@
 #ifndef ERROR_H
 # define ERROR_H
 
-# define PIPE_MSG "oh no pipe is broken"
-# define QUOTE_MSG "open quote"
-# define SYNTAX_MSG "quote unclosed"
-# define SYNTAX_ERROR "minishell: syntax error"
+# define PIPE_MSG "syntax error near unexpected token `|'"
+# define QUOTE_MSG "unexpected EOF while looking for matching `\"'"
+# define SYNTAX_MSG "syntax error: unexpected end of file"
+# define SYNTAX_ERROR "syntax error near unexpected token"
+# define REDIRECT_MSG "syntax error near unexpected token `newline'"
+# define OPEN_MSG "syntax error: unexpected end of file, expected `)'"
+# define CLOSE_MSG "syntax error near unexpected token `)'"
 
 #endif
