2c14484fd7db01604faac0f291b1df2c97171fd8
Niklas Weber <nweber@student.42Heilbronn.de>
2025-08-29 11:34:03 +0200

    first logic tree idea

---
+++ srcs/logic_tree/logic_tree.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   logic_tree.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/29 11:19:18 by nweber            #+#    #+#             */
+/*   Updated: 2025/08/29 11:24:44 by nweber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	*build_tree(t_shell_data *shell, t_list *tokens)
+{
+	(void)shell;
+	(void)tokens;
+	return (NULL);
+}
+
+void insert_left_node(t_shell_data *shell, void *left_node, t_list *tokens)
+{
+	(void)shell;
+	(void)left_node;
+	(void)tokens;
+}
+
+void	create_and(t_shell_data *shell, void *left, void *right)
+{
+	(void)shell;
+	(void)left;
+	(void)right;
+}
+
+void	create_or(t_shell_data *shell, void *left, void *right)
+{
+	(void)shell;
+	(void)left;
+	(void)right;
+}
+
+void	build_subtree(t_shell_data *shell, t_list *tokens)
+{
+	(void)shell;
+	(void)tokens;
+}
