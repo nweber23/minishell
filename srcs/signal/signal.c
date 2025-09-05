31006caea47426fb32e9691d23d55f159b29db0f
Niklas Weber <nweber@student.42Heilbronn.de>
2025-09-05 19:43:18 +0200

    added signal handling

---
+++ srcs/signal/signal.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nweber <nweber@student.42Heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/05 19:26:32 by nweber            #+#    #+#             */
+/*   Updated: 2025/09/05 19:34:12 by nweber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	handle_sigint(int sig)
+{
+	if (sig == SIGINT)
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+		exit_code(130);
+	}
+}
+
+void	ignore_pipe(int sig)
+{
+	if (sig == SIGPIPE)
+		return ;
+}
+
+void	init_signals(void)
+{
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGPIPE, ignore_pipe);
+}
+
+void	interavtive_signals(void)
+{
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	reset_child_signals(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+}
