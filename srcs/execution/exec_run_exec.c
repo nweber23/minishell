2c652257732f647322d0aae438de36d8f3f56828
Callmeduy14 <yudiaje14@gmail.com>
2025-09-09 12:28:09 +0200

    execution back to before change

--- srcs/execution/exec_run_exec.c
+++ srcs/execution/exec_run_exec.c
@@ -6,7 +6,7 @@
 /*   By: yyudi <yyudi@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 12:01:30 by yyudi             #+#    #+#             */
-/*   Updated: 2025/09/09 11:25:42 by yyudi            ###   ########.fr       */
+/*   Updated: 2025/09/09 12:26:47 by yyudi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,12 +78,10 @@ static int	exec_external(t_shell_data *sh, t_cmd *cmd)
 	return (126);
 }
 
-
 static int run_builtin_parent(t_shell_data *sh, t_cmd *cmd)
 {
     t_fdpack p;
     int status;
-	char	**expanded;
 
     fdpack_init(&p);
     if (apply_all_redirs(cmd, &p.in, &p.out) != 0)
@@ -92,49 +90,29 @@ static int run_builtin_parent(t_shell_data *sh, t_cmd *cmd)
         return (1);
     }
     fd_apply_inout(&p);
-	expanded = expand_argv_if_needed(cmd->argv);
-	if (expanded != NULL)
-		cmd->argv = expanded;
     status = exec_builtin(sh, cmd->argv);
     fd_restore(&p);
     return (status);
 }
 
-static void	child_exec(t_shell_data *sh, t_node *node, int in_fd, int out_fd)
+static void	child_exec(t_shell_data *sh, t_node *node, int fds[2])
 {
 	t_fdpack	p;
 	char		**expanded;
-	int			status;
 
 	reset_child_signals();
 	fdpack_init(&p);
-	p.in = in_fd;
-	p.out = out_fd;
+	p.in = fds[0];
+	p.out = fds[1];
 	if (apply_all_redirs(node->cmd, &p.in, &p.out) != 0)
 		exit(1);
-	if (p.in != -1)
-	{
-		dup2(p.in, STDIN_FILENO);
-		close(p.in);
-	}
-	if (p.out != -1)
-	{
-		dup2(p.out, STDOUT_FILENO);
-		close(p.out);
-	}
+	fd_apply_inout(&p);
 	expanded = expand_argv_if_needed(node->cmd->argv);
 	if (expanded != NULL)
 		node->cmd->argv = expanded;
-	if (node->cmd->argv && is_builtin(node->cmd->argv[0]) != 0)
-	{
-		status = exec_builtin(sh, node->cmd->argv);
-		exit(status);
-	}
-	status = exec_external(sh, node->cmd);
-	exit(status);
+	exec_external(sh, node->cmd);
+	exit(127);
 }
-
-
 
 
 static int	wait_and_status(pid_t pid)
@@ -154,39 +132,22 @@ int	run_exec_node(t_shell_data *sh, t_node *node, int fds[2], int is_top)
 {
 	pid_t		pid;
 	const char	*name;
-	int			in_fd;
-	int			out_fd;
 
-	if (!node || !node->cmd)
+	if (!node->cmd)
 		return (1);
 	name = NULL;
 	if (node->cmd->argv)
 		name = node->cmd->argv[0];
-	if (is_top && name && is_builtin(name) != 0)
+	if (is_top && name && is_builtin(name))
 		return (run_builtin_parent(sh, node->cmd));
-
-	in_fd = -1;
-	out_fd = -1;
-	if (fds != NULL)
-	{
-		if (fds[0] != -1)
-			in_fd = fds[0];
-		if (fds[1] != -1)
-			out_fd = fds[1];
-	}
 	pid = fork();
 	if (pid == -1)
 		return (perror("fork"), 1);
 	if (pid == 0)
-		child_exec(sh, node, in_fd, out_fd);
-
-	/* parent: TUTUP kedua ujung pipe kalau ada */
-	if (fds != NULL)
-	{
-		if (fds[0] != -1)
-			close(fds[0]);
-		if (fds[1] != -1)
-			close(fds[1]);
-	}
+		child_exec(sh, node, fds);
+	if (fds[0] != -1 && fds[0] != STDIN_FILENO)
+		close(fds[0]);
+	if (fds[1] != -1 && fds[1] != STDOUT_FILENO)
+		close(fds[1]);
 	return (wait_and_status(pid));
 }
