8b336db5af3922fca1ed5208cf38828f71219f37
Niklas Weber <nweber@student.42Heilbronn.de>
2025-09-11 18:50:45 +0200

    fixed error_code return

--- srcs/main.c
+++ srcs/main.c
@@ -2,62 +2,63 @@
 
 static void create_env_list(t_shell_data *sh, char **envp)
 {
-    int i = 0;
+	int i = 0;
 
-    sh->env = NULL;
-    while (envp && envp[i])
-    {
-        char *eq = ft_strchr(envp[i], '=');
-        if (eq)
-        {
-            size_t klen = (size_t)(eq - envp[i]);
-            char  *key  = ft_substr(envp[i], 0, klen);
-            char  *val  = ft_strdup(eq + 1);
-            if (!key || !val || env_set(sh, key, val) != 0)
-                error_malloc("create_env_list", sh);
-            free(key);
-            free(val);
-        }
-        i++;
-    }
+	sh->env = NULL;
+	while (envp && envp[i])
+	{
+		char *eq = ft_strchr(envp[i], '=');
+		if (eq)
+		{
+			size_t klen = (size_t)(eq - envp[i]);
+			char  *key  = ft_substr(envp[i], 0, klen);
+			char  *val  = ft_strdup(eq + 1);
+			if (!key || !val || env_set(sh, key, val) != 0)
+				error_malloc("create_env_list", sh);
+			free(key);
+			free(val);
+		}
+		i++;
+	}
 }
 
 static void bump_shlvl(t_shell_data *sh)
 {
-    const char *cur;
-    int val;
-    char *s;
+	const char *cur;
+	int val;
+	char *s;
 
-    cur = env_get(sh->env, "SHLVL");
-    if (cur == NULL)
-        val = 0;
-    else
-        val = ft_atoi(cur);
-    s = ft_itoa(val + 1);
-    if (!s)
-        error_malloc("bump_shlvl", sh);
-    if (env_set(sh, "SHLVL", s) != 0)
-        error_malloc("bump_shlvl:env_set", sh);
-    free(s);
+	cur = env_get(sh->env, "SHLVL");
+	if (cur == NULL)
+		val = 0;
+	else
+		val = ft_atoi(cur);
+	s = ft_itoa(val + 1);
+	if (!s)
+		error_malloc("bump_shlvl", sh);
+	if (env_set(sh, "SHLVL", s) != 0)
+		error_malloc("bump_shlvl:env_set", sh);
+	free(s);
 }
 
 int main(int ac, char **av, char **envp)
 {
-    t_shell_data sh;
+	t_shell_data	sh;
+	int				ret;
 
-    (void)av;
-    ft_memset(&sh, 0, sizeof(sh));
-    if (ac > 1)
-    {
-        ft_putendl_fd("i dont want arguments", 2);
-        return (EXIT_FAILURE);
-    }
-    create_env_list(&sh, envp);
-    bump_shlvl(&sh);
-    minishell_loop(&sh, envp);
-    if (sh.env_array)
-        ft_array_free(sh.env_array);
-    free_shell(&sh);
-    free_env(sh.env);
-    return (0);
+	(void)av;
+	ft_memset(&sh, 0, sizeof(sh));
+	if (ac > 1)
+	{
+		ft_putendl_fd("i dont want arguments", 2);
+		return (EXIT_FAILURE);
+	}
+	create_env_list(&sh, envp);
+	bump_shlvl(&sh);
+	ret = minishell_loop(&sh, envp);
+	if (sh.env_array)
+		ft_array_free(sh.env_array);
+	free_shell(&sh);
+	free_env(sh.env);
+	return (ret);
 }
