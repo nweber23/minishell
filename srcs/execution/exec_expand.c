f11110e6e8822ea1d6a1c3e58078ae80c3d7dc5e
Niklas Weber <nweber@student.42Heilbronn.de>
2025-09-17 16:44:57 +0200

    fixed wildcards not being detected when trying to run with a directory

--- srcs/execution/exec_expand.c
+++ srcs/execution/exec_expand.c
@@ -12,45 +12,81 @@
 
 #include "execution.h"
 
-static int	push_name(char ***dst_vec, int *dst_cnt, const char *name)
-{
-	char	*dup_name;
+// static int	push_name(char ***dst_vec, int *dst_cnt, const char *name)
+// {
+// 	char	*dup_name;
 
-	dup_name = ft_strdup(name);
-	if (!dup_name)
-		return (0);
-	if (!append_word(dst_vec, dst_cnt, dup_name))
-	{
-		free(dup_name);
-		return (0);
-	}
-	return (1);
-}
+// 	dup_name = ft_strdup(name);
+// 	if (!dup_name)
+// 		return (0);
+// 	if (!append_word(dst_vec, dst_cnt, dup_name))
+// 	{
+// 		free(dup_name);
+// 		return (0);
+// 	}
+// 	return (1);
+// }
 
 static int	build_match_list(const char *pattern, char ***out_vec, int *out_cnt)
 {
 	DIR				*dir_ptr;
 	struct dirent	*dir_ent;
-	char			*entry_name;
+	const char		*name_pattern;
+	char			*dirpath;
+	const char		*slash;
+	char			*full_entry;
+	size_t			dlen;
 
 	*out_vec = NULL;
 	*out_cnt = 0;
-	dir_ptr = opendir(".");
-	if (!dir_ptr)
+	slash = ft_strrchr(pattern, '/');
+	if (slash)
+	{
+		dlen = (size_t)(slash - pattern);
+		if (dlen == 0)
+			dirpath = ft_strdup("/");
+		else
+			dirpath = ft_substr(pattern, 0, dlen);
+		name_pattern = slash + 1;
+	}
+	else
+	{
+		dirpath = ft_strdup(".");
+		name_pattern = pattern;
+	}
+	if (!dirpath)
 		return (0);
+	if (*name_pattern == '\0')
+		name_pattern = "*";
+	dir_ptr = opendir(dirpath);
+	if (!dir_ptr)
+		return (free(dirpath), 0);
 	dir_ent = readdir(dir_ptr);
 	while (dir_ent)
 	{
-		entry_name = dir_ent->d_name;
-		if (!should_skip_hidden(pattern, entry_name)
-			&& glob_match(pattern, entry_name))
+		if (!should_skip_hidden(name_pattern, dir_ent->d_name)
+			&& glob_match(name_pattern, dir_ent->d_name))
 		{
-			if (!push_name(out_vec, out_cnt, entry_name))
-				return (closedir(dir_ptr), ft_array_free(*out_vec), 0);
+			if (ft_strcmp(dirpath, ".") == 0)
+				full_entry = ft_strdup(dir_ent->d_name);
+			else if (ft_strcmp(dirpath, "/") == 0)
+				full_entry = ft_str3var("/", dir_ent->d_name, "");
+			else
+				full_entry = ft_str3var(dirpath, "/", dir_ent->d_name);
+			if (!full_entry || !append_word(out_vec, out_cnt, full_entry))
+			{
+				if (full_entry)
+					free(full_entry);
+				closedir(dir_ptr);
+				ft_array_free(*out_vec);
+				free(dirpath);
+				return (0);
+			}
 		}
 		dir_ent = readdir(dir_ptr);
 	}
 	closedir(dir_ptr);
+	free(dirpath);
 	return (1);
 }
 
@@ -69,8 +105,8 @@ static int	append_copy(char ***dst_vec, int *dst_cnt, const char *word)
 	return (1);
 }
 
-static int	expand_pattern_word(const char *pattern,
-		char ***dst_vec, int *dst_cnt)
+static int	expand_pattern_word(const char *pattern, char ***dst_vec,
+		int *dst_cnt)
 {
 	char	**match_vec;
 	int		match_cnt;
@@ -112,8 +148,8 @@ char	**expand_argv_if_needed(char **argv)
 	{
 		if (has_star(argv[source_index]))
 		{
-			if (!expand_pattern_word(argv[source_index],
-					&result_vec, &result_count))
+			if (!expand_pattern_word(argv[source_index], &result_vec,
+					&result_count))
 				return (NULL);
 		}
 		else if (!append_copy(&result_vec, &result_count, argv[source_index]))
